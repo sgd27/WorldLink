f4e8627e546c7c7610cd0e7d54ef0c69110a4bd8
施国栋 <sgd322@163.com>
2018-02-27 17:05:51 +0800

    update

--- App/Redux/CreateStore.js
+++ App/Redux/CreateStore.js
@@ -1,22 +1,29 @@
-import { createStore, applyMiddleware, compose } from 'redux'
 import Config from '../Config/DebugConfig'
-import createSagaMiddleware from 'redux-saga'
 import ScreenTracking from './ScreenTrackingMiddleware'
+import {createReactNavigationReduxMiddleware} from 'react-navigation-redux-helpers'
+import {applyMiddleware, compose, createStore} from 'redux'
+import createSagaMiddleware from 'redux-saga'
 
 // creates the store
-export default (rootReducer, rootSaga) => {
+export default(rootReducer, rootSaga) => {
   /* ------------- Redux Configuration ------------- */
 
   const middleware = []
   const enhancers = []
+
+  /* ------------- Navigation Middlewate ------------ */
+  const navigationMiddleware = createReactNavigationReduxMiddleware('root', state => state.nav)
+  middleware.push(navigationMiddleware)
 
   /* ------------- Analytics Middleware ------------- */
   middleware.push(ScreenTracking)
 
   /* ------------- Saga Middleware ------------- */
 
-  const sagaMonitor = Config.useReactotron ? console.tron.createSagaMonitor() : null
-  const sagaMiddleware = createSagaMiddleware({ sagaMonitor })
+  const sagaMonitor = Config.useReactotron
+    ? console.tron.createSagaMonitor()
+    : null
+  const sagaMiddleware = createSagaMiddleware({sagaMonitor})
   middleware.push(sagaMiddleware)
 
   /* ------------- Assemble Middleware ------------- */
@@ -24,15 +31,13 @@ export default (rootReducer, rootSaga) => {
   enhancers.push(applyMiddleware(...middleware))
 
   // if Reactotron is enabled (default for __DEV__), we'll create the store through Reactotron
-  const createAppropriateStore = Config.useReactotron ? console.tron.createStore : createStore
+  const createAppropriateStore = Config.useReactotron
+    ? console.tron.createStore
+    : createStore
   const store = createAppropriateStore(rootReducer, compose(...enhancers))
 
   // kick off root saga
   let sagasManager = sagaMiddleware.run(rootSaga)
 
-  return {
-    store,
-    sagasManager,
-    sagaMiddleware
-  }
+  return {store, sagasManager, sagaMiddleware}
 }
